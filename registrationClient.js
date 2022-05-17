ae2c1a9125da105a67692cfdf7a231a7049a4594
rohit_sikrewal <sikrewalrohit96@gmail.com>
2022-05-16 18:59:27 -0700

    changes to registration

--- registrationClient.js
+++ registrationClient.js
@@ -3,6 +3,16 @@ const PROTO_PATH_GATEWAY = './proto/gateway-comm.proto';
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const _ = require('lodash');
+const redis = require('redis');
+var ip = require("ip");
+
+// const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+// const redis_client = redis.createClient(REDIS_PORT);
+
+// // set data on redis
+// redis_client.setEx("token", 3600, "123456");
+
 
 let packageDefinition_gateway = protoLoader.loadSync(
     PROTO_PATH_GATEWAY,
@@ -13,40 +23,43 @@ let packageDefinition_gateway = protoLoader.loadSync(
       oneofs: true
 });
 
-let gateway_comm_proto = grpc.loadPackageDefinition(packageDefinition_gateway).stream;
-
-function main() {
-    let registrationClient = new gateway_comm_proto.Authenticate('localhost:4500',
-                                         grpc.credentials.createInsecure());
-
-    var registrationCreds = {
-        name : "nodeJs_Node",
-        password : "cmpe275",
-    }
-
-    // client_ip is ip of application client or this client ?
-    var loginCreds = {
-      client_ip: "client ip",
-      password : "cmpe275",
-    }
-
-    registrationClient.Register(registrationCreds, (error, response) => {
-      if (!error){
-        console.log(response.masterip);
-      } else {
-        console.error(error);
-      }
-    });
-    
-    registrationClient.Login(loginCreds, (error, response) => {
-      if (!error){
-        console.log(response.msg);
-        console.log(response.token);
-      } else {
-        console.error(error);
-      }
-    });
+let gateway_comm_proto = grpc.loadPackageDefinition(packageDefinition_gateway).stream;  
                                          
-  }
+// function main() {
+
+//     let registrationClient = new gateway_comm_proto.Authenticate('localhost:4500',
+//                                          grpc.credentials.createInsecure());
+
+//     var ipa = ip.address(); //our ip
+
+//     var registrationCreds = {
+//         name : "nodeJs_Node",
+//         password : "cmpe275",
+//     }
+
+//     // client_ip is ip of application client or this client ?
+//     var loginCreds = {
+//       client_ip: "client ip",
+//       password : "cmpe275",
+//     }
+
+//     registrationClient.Register(registrationCreds, (error, response) => {
+//       if (!error){
+//         console.log(response.masterip);
+//       } else {
+//         console.error(error);
+//       }
+//     });
+    
+//     registrationClient.Login(loginCreds, (error, response) => {
+//       if (!error){
+//         console.log(response.msg);
+//         console.log(response.token);
+//       } else {
+//         console.error(error);
+//       }
+//     });
+                                         
+//   }
   
-main(); 
+// main();
